6613c6d1569a6d8e1eb4f4f11b071e62739baf81
gcp5678 <40751521+gcp5678@users.noreply.github.com>
2021-11-27 05:33:55 +0800

    f

--- cert.sh
+++ cert.sh
@@ -1,8 +1,1 @@
-apt-get install -y openssl cron socat curl unzip vim wget git
-&& curl https://get.acme.sh | sh -s email=sgfhsffd@gmail.com 
-&& ~/.acme.sh/acme.sh  --issue -d xxx  --standalone 
-&& ~/.acme.sh/acme.sh --installcert -d xjp.125125.xyz --fullchainpath /etc/ssl/private/smitho.crt --keypath /etc/ssl/private/smitho.key --ecc 
-&&chmod 755 /etc/ssl/private 
-&& chmod 755 /etc/ssl/private/smitho.crt 
-&& chmod 755 /etc/ssl/private/smitho.key 
-&& ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+apt-get install -y openssl cron socat curl unzip vim wget git && curl https://get.acme.sh | sh -s email=sgfhsffd@gmail.com && ~/.acme.sh/acme.sh  --issue -d xxx  --standalone && ~/.acme.sh/acme.sh --installcert -d xjp.125125.xyz --fullchainpath /etc/ssl/private/smitho.crt --keypath /etc/ssl/private/smitho.key --ecc &&chmod 755 /etc/ssl/private && chmod 755 /etc/ssl/private/smitho.crt && chmod 755 /etc/ssl/private/smitho.key && ~/.acme.sh/acme.sh --upgrade --auto-upgrade
