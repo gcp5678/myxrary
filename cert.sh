a05188c0c47ebd908e2070a55ed9be7eedc749eb
gcp5678 <40751521+gcp5678@users.noreply.github.com>
2021-11-27 04:25:15 +0800

    1

---
+++ cert.sh
@@ -0,0 +1,7 @@
+apt-get install -y openssl cron socat curl unzip vim wget git
+
+curl https://get.acme.sh | sh -s email=sgfhsffd@gmail.com
+~/.acme.sh/acme.sh  --issue -d xxx  --standalone -k ec-256//把域名换成自己的
+~/.acme.sh/acme.sh --installcert -d xxx --fullchainpath /etc/ssl/private/smitho.crt --keypath /etc/ssl/private/smitho.key --ecc
+chmod 755 /etc/ssl/private && chmod 755 /etc/ssl/private/smitho.crt && chmod 755 /etc/ssl/private/smitho.key
+~/.acme.sh/acme.sh --upgrade --auto-upgrade  
